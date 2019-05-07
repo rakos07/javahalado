b71d4ea39ea8665f17e186d51697764ccbd54212
rakos07 <rakos07@gmail.com>
2019-05-07 21:41:05 +0200

    with jarmu

---
+++ jarmu_values/src/main/java/hu/me/OutputJarmu.java
@@ -0,0 +1,21 @@
+package hu.me;
+public class OutputJarmu {
+
+    private int osszesuly = 0;
+    private JarmuHiba jarmuhiba;
+
+    public JarmuHiba getJarmuhiba() {
+        return jarmuhiba;
+    }
+
+    public void setJarmuhiba(JarmuHiba jarmuhiba) {
+        this.jarmuhiba = jarmuhiba;
+    }
+
+    public int getOsszesuly() {
+        return osszesuly;
+    }
+    public void setOsszesuly(int osszesuly) {
+        this.osszesuly = osszesuly;
+    }
+}
